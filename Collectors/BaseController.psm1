6315d241058bb9b1a0bd671431ba2dcb0eedbeed
LHazy <fhazy2255@gmail.com>
2025-06-30 23:23:11 -0600

    結果の書き出し処理

--- Collectors/BaseController.psm1
+++ Collectors/BaseController.psm1
@@ -1,9 +1,10 @@
 Class BaseCollector {
     [hashtable] $globalConfig = @{}
     [hashtable] $controllerConfig = @{}
-    [string[]] $requireScopes = @()
     [string] $iniSection = ""
     [boolean] $isEnabled = $false
+    [string[]] $requireScopes = @()
+    [string] $outputFileName = ""
 
     BaseCollector() { }
 
@@ -11,7 +12,20 @@ Class BaseCollector {
         $this.controllerConfig = $this.globalConfig[$this.iniSection]
         $this.isEnabled = $this.controllerConfig["Enabled"]
         $this.requireScopes = $this.controllerConfig["RequireScopes"]
+
+        if (-not $this.controllerConfig.ContainsKey("OutputFileName")) {
+            $this.outputFileName = "$($this.iniSection).json"
+        } else {
+            $this.outputFileName = $this.controllerConfig["OutputFileName"]
+        }
     }
 
-    DoCollect() { }
+    WriteResult($result) {
+        $outputFolder = $this.globalConfig["OutputFolderPath"]
+        $outputFile = Join-Path -Path $outputFolder -ChildPath $this.outputFileName
+        if (-not (Test-Path -Path $outputFolder)) {
+            New-Item -Path $outputFolder -ItemType Directory | Out-Null
+        }
+        $result | ConvertTo-Json -Depth 10 | Out-File -FilePath $outputFile -Encoding utf8 -Force
+    }
 }
